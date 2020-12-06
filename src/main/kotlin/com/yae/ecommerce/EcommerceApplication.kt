41f1fa92dc1e9db06fd2ace11eadb864e8fbb53b
vcnt72 <vincentsanjaya72@gmail.com>
2020-12-06 20:18:03 +0700

    Initial commit

---
+++ src/main/kotlin/com/yae/ecommerce/EcommerceApplication.kt
@@ -0,0 +1,11 @@
+package com.yae.ecommerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommerceApplication
+
+fun main(args: Array<String>) {
+    runApplication<EcommerceApplication>(*args)
+}
